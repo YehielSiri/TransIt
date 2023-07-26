501fb201553e214b88c82aa5586cdd726b24617b
YehielSiri <yehiels14@gmail.com>
2023-07-27 02:02:28 +0300

    Authentication - build the secret token

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -2,6 +2,7 @@ const {User} = require('../models/user');
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 
 router.get(`/`, async (req, res) =>{
@@ -50,9 +51,26 @@ router.post(`/`, async (req, res) =>{
 
 router.post('/login', async(req, res) =>{
     const user = await User.findOne({email: req.body.email})
+    const secret = process.env.secret;
 
     if(!user) {
         return res.status(400).send('The user not found.')
+    }
+
+    // Authenticate
+    if(user && bcrypt.compareSync(req.body.password, user.passwordHash)){
+        // User has already authenticated
+        const token = jwt.sign(
+            {
+                userId: user.id
+            },
+            secret,
+            {expiresIn: '1w'}
+        )
+
+        res.status(200).send({user: user.email, token: token})
+    } else {
+        res.status(400).send('Password is wrong!')
     }
 
     return res.status(200).send(user);
