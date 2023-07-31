b3a8714f9a4166b0d552f19690a510fa82a9b6c9
YehielSiri <yehiels14@gmail.com>
2023-07-31 16:40:12 +0300

    trunk.js

--- backend/models/trunk.js
+++ backend/models/trunk.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
 const trunkSchema = mongoose.Schema({
-    courior: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: [true, "There is no trunk of nobody"],
-    },
     trunkItems: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'trunkItem',
         required: [true, "An empty trunk worth nothing"]
     }],
+    courior: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, "There is no trunk of nobody"],
+    },
     totalPrice: {
         type: Number,
         required: true,
@@ -22,7 +22,7 @@ const trunkSchema = mongoose.Schema({
     status: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'StatusTrunk',
-        default: 'closed',
+        // default: 'closed', // How am I init a default value through ObjectID
     },
 })
 
@@ -38,3 +38,26 @@ trunkSchema.set('toJSON', {
 });
 
 exports.Trunk = mongoose.model('Trunk', trunkSchema);
+
+
+/**
+Trunk Example:
+
+{
+    "trunkItems" : [
+        {
+            "quantity": 3,
+            "product" : "5fcfc406ae79b0a6a90d2585"
+        },
+        {
+            "quantity": 2,
+            "product" : "5fd293c7d3abe7295b1403c4"
+        }
+    ],
+    "courior": "5fd51bc7e39ba856244a3b44",
+    "totalPrice": "00000",
+    "dateCreated": "Czech Republic",
+    "status": "+420702241333",
+}
+
+ */
